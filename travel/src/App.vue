200c8622e0691d738c021116bb03b8a98edd0442
qiaoyue77 <1660285725@qq.com>
2021-09-24 15:42:37 +0800

    'start'

---
+++ travel/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <router-view/>
+<!--    多页面应用 请求对应的HTML文件
+    首屏时间快
+    SEO优化好 查看到所有代码
+    每次切换比较慢
+    单页面应用
+    router-link
+    seo差 nuxt.js
+
+    minimum-scale=1.0;maximum-scale=1.0;user-scalable=no
+
+    引入reset.css
+    import './assets/styles/reset.css'
+    border.css
+    devicePixelRatio -> 像素比
+
+    双击放大页面 -> 用户第一次点击 用户第二次点击
+    点击跳转链接 第一次点击后等待 是否第二次点击
+    300ms延迟
+    fastclick.js
+
+    iconfont
+    -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
