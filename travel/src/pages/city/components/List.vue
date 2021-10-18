3edb42a0dae065c6699daf29648770dad579e054
qiaoyue77 <1660285725@qq.com>
2021-10-18 15:56:05 +0800

    'city-detail'

--- travel/src/pages/city/components/List.vue
+++ travel/src/pages/city/components/List.vue
@@ -7,8 +7,47 @@
         <li>北京</li>
       </ul>
     </div>
-    <div class="alphabet-menu"></div>
-    <div class="alphabet-list"></div>
+    <div class="alphabet-menu">
+      <div class="alphabet-menu-title">字母排序</div>
+      <ul class="clearfix">
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+      </ul>
+    </div>
+    <div class="alphabet-list">
+      <div class="alphabet-list-title">A</div>
+      <ul class="clearfix">
+        <li>阿拉</li>
+        <li>阿拉</li>
+        <li>阿拉</li>
+        <li>阿拉</li>
+        <li>阿拉</li>
+      </ul>
+    </div>
   </div>
 </div>
 </template>
@@ -29,6 +68,15 @@ export default {
 div{
   box-sizing :border-box;
 }
+.hot-cts-title,
+.alphabet-list-title,
+.alphabet-menu-title{
+  padding:0 .2rem;
+  height :.72rem;
+  line-height :.72rem;
+  background-color :#f5f5f5
+}
+
 .hot-cities{
   width:100%;
   height:4.32rem;
@@ -52,5 +100,33 @@ div{
   font-size:.23rem;
   background-color :orange;
 
+}
+  .alphabet-menu{
+    height:5.0rem;
+    margin:.3rem 0;
+    .alphabet-menu-title{
+      font-size:0.25rem;
+    }
+    li{
+      float:left;
+      width:16.67%;
+      height:.9rem;
+      line-height :.9rem;
+      text-align :center;
+      font-size:0.25rem;
+    }
+  }
+.alphabet-list{
+  .alphabet-list-title{
+    font-size:0.25rem;
+  }
+  li{
+    float:left;
+    width:25%;
+    height:.9rem;
+    line-hegiht:.9rem;
+    text-align center;
+    font-size:0.25rem;
+  }
 }
 </style>
